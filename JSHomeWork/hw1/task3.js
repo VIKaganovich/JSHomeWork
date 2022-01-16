6ce86a1d388709c9a2643ce3e5a88e9958657804
Vitalij Kaganovich <vitalij.kaganovich@privatbank.ua>
2022-01-16 11:20:32 +0200

    commit fixed task3

--- JSHomeWork/hw1/task3.js
+++ JSHomeWork/hw1/task3.js
@@ -1,6 +1,6 @@
 if (val == 'a') {
     console.log('a');
-} else if (val >= 'b' && val <= 'e') {
+} else if (val && val.length == 1 && val >= 'b' && val <= 'e') {
     console.log('others');
 } else {
     console.log('unknown');
